3d8ac3e13b39c0d60d20cb9baf491cda7d6590c9
Lucariatias <lucariatias@gmail.com>
2014-01-23 17:25:41 +0000

    Format source

--- src/main/java/io/github/lucariatias/realchop/RealChop.java
+++ src/main/java/io/github/lucariatias/realchop/RealChop.java
@@ -5,46 +5,46 @@ import org.bukkit.configuration.file.FileConfiguration;
 import java.lang.String;
 import java.io.File;
 
-public class RealChop extends JavaPlugin{
-    public FileConfiguration config;
-	 
+public class RealChop extends JavaPlugin {
+	public FileConfiguration config;
+
 	public void onEnable() {
 		config = this.getConfig();
-        config.options().configuration().addDefault("DetectBlockBreak","all");
-        config.options().configuration().addDefault("BlockProcessingLimit",150);
-        config.options().configuration().addDefault("FallingLeaves",true);
+		config.options().configuration().addDefault("DetectBlockBreak", "all");
+		config.options().configuration().addDefault("BlockProcessingLimit", 150);
+		config.options().configuration().addDefault("FallingLeaves", true);
 		File dir = this.getDataFolder();
 		File file = new File(dir, "config.yml");
 		if (!file.exists()) {
-			//creating new configuration
+			// creating new configuration
 			FileConfiguration config = this.getConfig();
-			String eol = System.getProperty("line.separator");  
-	        config.options().copyDefaults(true);
-	        String header; 
-	        header = "RealChop Properties: " + eol;
-	        header += eol;
-	        header +="DetectBlockBreak log | leaves | all" + eol;
-	        header +="  Tree will fall (after physics check), when player break this type of blocks:" + eol;
-	        header +="  log - only LOG blocks (tree parts)" + eol;
-			header +="  leaves - LOG and LEAVES blocks" + eol;
-			header +="  all - any blocks" + eol;
-			header +="  default: all" + eol;
+			String eol = System.getProperty("line.separator");
+			config.options().copyDefaults(true);
+			String header;
+			header = "RealChop Properties: " + eol;
 			header += eol;
-			header +="BlockProcessingLimit num " + eol;
-			header +="  num (5 - 1000) - setting limit of blocks for physics calculation." + eol;
-			header +="  Setting high limit can produce heavy server load, when process massive structures, builded from LOG blocks." + eol;
-			header +="  default: 150" + eol;
+			header += "DetectBlockBreak log | leaves | all" + eol;
+			header += "  Tree will fall (after physics check), when player break this type of blocks:" + eol;
+			header += "  log - only LOG blocks (tree parts)" + eol;
+			header += "  leaves - LOG and LEAVES blocks" + eol;
+			header += "  all - any blocks" + eol;
+			header += "  default: all" + eol;
 			header += eol;
-			header +="FallingLeaves true | false" + eol;
+			header += "BlockProcessingLimit num " + eol;
+			header += "  num (5 - 1000) - setting limit of blocks for physics calculation." + eol;
+			header += "  Setting high limit can produce heavy server load, when process massive structures, builded from LOG blocks." + eol;
+			header += "  default: 150" + eol;
+			header += eol;
+			header += "FallingLeaves true | false" + eol;
 			header += "  Process LEAVES blocks, or just LOG blocks." + eol;
-			header +="  default: false" + eol;
-	        config.options().header(header);
-	        this.saveConfig();
+			header += "  default: false" + eol;
+			config.options().header(header);
+			this.saveConfig();
 		}
 		getServer().getPluginManager().registerEvents(new RealChopListener(this), this);
 	}
- 
-	public void onDisable(){
-		//getLogger().info("Your plugin has been disabled!");
+
+	public void onDisable() {
+		// getLogger().info("Your plugin has been disabled!");
 	}
 }
